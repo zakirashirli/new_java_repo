d9216917eda22f4d4456ceca43361750a1d76a53
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2025-06-17 12:49:04 +0400

    Created new methods

--- my_java_projects/src/exercises/MyToyShop.java
+++ my_java_projects/src/exercises/MyToyShop.java
@@ -6,10 +6,11 @@ public class MyToyShop {
         product1.id = 1;
         product1.name = "car toy";
         product1.price = 4.99;
-        product1.rating = 3;
-        product1.age_restriction = "3-8";
+        product1.rating = 11; // 8/10
+        product1.age_restriction = 3;
         product1.product_info();
-
+        product1.setAge_restriction(3);
+        product1.evaluateRating();
     }
 }
 
@@ -18,8 +19,9 @@ class MyProducts {
     int id;
     String name;
     double price;
-    int rating;
-    String age_restriction;
+    int rating; // po 10 balnoy
+    int age_restriction;
+    int child_age;
 
     void product_info() {
         System.out.println("id: " + id);
@@ -27,5 +29,23 @@ class MyProducts {
         System.out.println("price: " + price);
         System.out.println("rating: " + rating);
         System.out.println("age_restriction: " + age_restriction);
+    }
+
+    void setAge_restriction(int child_age) {
+       if (child_age < age_restriction) {
+           System.out.println("Ребенку меньше " + age_restriction + ", Покупка запрещена!");
+       } else{
+           System.out.println("Возрост Подходит!");
+       }
+    }
+
+    void evaluateRating(){
+        if(rating >= 7) {
+            System.out.println("Товар Отличный!");
+        } else if(rating >= 5) {
+            System.out.println("Товар не плохой!");
+        } else {
+            System.out.println("Низкий Рейтинг!");
+        }
     }
 }

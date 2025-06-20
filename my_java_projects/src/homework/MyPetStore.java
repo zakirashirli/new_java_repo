5dc2723280e98a58c057ea1a49379c19a2d943e8
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2025-06-20 11:28:38 +0400

    update

---
+++ my_java_projects/src/homework/MyPetStore.java
@@ -0,0 +1,18 @@
+package homework;
+
+public class MyPetStore {
+}
+
+class Pet {
+    int id;
+    String name;
+    String type;
+    String gender;
+    int price;
+    int age_restriction;
+    int customer_age;
+
+    void pet_info(){
+        //
+    }
+}
